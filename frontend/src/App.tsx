5b9f2e31214865c896dfae3e6f9d8bbd91e69df7
Jongu Yu <yujongu@gmail.com>
2023-05-12 10:07:28 +0900

    create ui for weather part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import BlockSquareBig from "./components/block-square-big";
 import clear from "../src/images/Weather Glassmorphism Icon/clear.png";
+import { dateToString } from "./helperFunctions/prettyDate";
 function App() {
+  const today = new Date();
+  console.log(today);
   return (
     <div className="App">
       <div id="topTitleBar">
-        <span>Weather stuff </span>
+        <div id="topTitleBar_TextContainer">
+          <p>어서오세요, ㅁㅁㅁ님!</p>
+          <p>{dateToString(today, 2)}</p>
+          <p>{dateToString(today, 1)}</p>
+          <p>서울시</p>
+          <p>31°C</p>
+        </div>
         <img src={clear} alt="clear" className="WeatherIcons" />
       </div>
       <BlockSquareBig title="This is food" />
