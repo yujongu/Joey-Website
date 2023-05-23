bdef2d13722b6cbbaa998a621cac667da0eaf2a1
Jongu Yu <yujongu@gmail.com>
2023-05-23 09:49:46 +0900

    add show password function

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,8 +1,19 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faHandshake } from "@fortawesome/free-regular-svg-icons";
 import { faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const navigateToSignup = () => {
+    navigate("/signup");
+  };
+
   return (
     <div className="LoginContainer">
       <div className="LoginHeader">
@@ -22,11 +33,20 @@ function Login() {
           <input
             className="loginInput"
             placeholder="Password"
-            type="password"
+            type={showPassword ? "text" : "password"}
           />
 
-          <label style={{ color: "white" }} className="disableTextSelect">
-            <input type="checkbox" id="showPw" name="showPassword" />
+          <label
+            style={{ color: "white" }}
+            className="disableTextSelect isButton"
+          >
+            <input
+              type="checkbox"
+              id="showPw"
+              name="showPassword"
+              className="isButton"
+              onClick={() => setShowPassword(!showPassword)}
+            />
             Show Password
           </label>
         </div>
@@ -34,8 +54,14 @@ function Login() {
         <input className="loginInput isButton" value="Login" type="button" />
       </div>
 
-      <div style={{ color: "var(--warning)" }}>Forgot Password?</div>
-      <div style={{ color: "var(--primary)", marginTop: "80px" }}>
+      <div className="isButton" style={{ color: "var(--warning)" }}>
+        Forgot Password?
+      </div>
+      <div
+        className="isButton"
+        style={{ color: "var(--primary)", marginTop: "80px" }}
+        onClick={navigateToSignup}
+      >
         Create your account!
       </div>
     </div>
