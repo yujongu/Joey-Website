2ce8594e72cec1a9b7e9cf0969f06ced4859521b
Jongu Yu <yujongu@gmail.com>
2023-05-23 09:49:20 +0900

    create Signup page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./routes/Main";
 import Login from "./routes/Login";
+import Signup from "./routes/Signup";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
