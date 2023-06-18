68e96af349e76f6e412d74a224bd715951845720
yujongu <yujongu@gmail.com>
2023-06-18 17:12:44 +0900

    add kakao map to date planner page

--- frontend/src/routes/DatePlanner.tsx
+++ frontend/src/routes/DatePlanner.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
 import "./DatePlanner.css";
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 function DatePlanner() {
-  return <div>Date Planner</div>;
+  useEffect(() => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(35.814, 127.0907),
+      level: 3,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div>
+      <div>Date Planner</div>;
+      <div id="map" style={{ width: "400px", height: "500px" }}></div>
+    </div>
+  );
 }
 export default DatePlanner;
